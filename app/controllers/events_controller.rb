04808ebfdbfae6e9a77d564966940c6016cc49fb
sarah niemiec <sroseniemiec@gmail.com>
2017-05-08 15:48:32 -0400

    can create an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,30 @@
 class EventsController < ApplicationController
   def index
   end
+
   def new
-    @event = Event.new 
+    @event = Event.new
   end
+
+  def create
+    puts "yooooooo" + event_params.inspect + current_user.id.inspect
+    @event = Event.new(event_params)
+    @event.user_id = current_user.id
+    if @event.save
+      flash[:notice] = "Your event was created!"
+      redirect_to root_path
+    else
+      flash[:alert] = "There was a problem creating your event."
+      render :new
+    end
+  end
+
+
+
+
+  # strong params
+  def event_params
+    params.require(:event).permit(:name, :description, :startdate, :enddate, :location, :cost, :image)
+  end
+
 end
