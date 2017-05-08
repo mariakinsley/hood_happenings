1688f73e52465c0f39ddaf984b055d6b7f9d5d31
Maria Kinsley <mariadfrancia2016@gmail.com>
2017-05-08 16:58:26 -0400

    validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,8 @@ class Event < ApplicationRecord
 
 has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>"}, default_url: "events_medium.jpg"
  validates_attachment_content_type :image, content_type:  /\Aimage\/.*\z/
+
+validates_presence_of :name, :description, :startdate, :enddate, :location, :cost
+validates :cost, numericality:{greater_than_or_equal_to: 0 }
+ validates :location, inclusion: {in: ["Noho","Soho","Lower East Side", "Upper West  side", "Upper East Side", "West Village", "East Village", "Hell's Kitchen", "Financial District", "Wall St.", "Midtown", "Harlem", "Washington Heights", "Chelsea", "Tribeca", "Chinatown", "Murray Hill"]}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
 
   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>"}, default_url: "events_medium.jpg"
  validates_attachment_content_type :photo, content_type:  /\Aimage\/.*\z/
+
+ validates :location, inclusion: {in: ["Noho","Soho","Lower East Side", "Upper West  side", "Upper East Side", "West Village", "East Village", "Hell's Kitchen", "Financial District", "Wall St.", "Midtown", "Harlem", "Washington Heights", "Chelsea", "Tribeca", "Chinatown", "Murray Hill"]}
 end
