91698f8c194fe3ab114698780f02a172837dbe8c
Maria Kinsley <mariadfrancia2016@gmail.com>
2017-05-08 15:19:42 -0400

    form for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>"}, default_url: "events_medium.jpg"
+ validates_attachment_content_type :image, content_type:  /\Aimage\/.*\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508182705) do
+ActiveRecord::Schema.define(version: 20170508184944) do
 
   create_table "events", force: :cascade do |t|
     t.integer  "user_id"
@@ -20,8 +20,12 @@ ActiveRecord::Schema.define(version: 20170508182705) do
     t.datetime "enddate"
     t.string   "location"
     t.decimal  "cost"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "posts", force: :cascade do |t|
