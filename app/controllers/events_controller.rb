2bc32421a6986faf39a539afd9b9ca72e3ab008c
sarah niemiec <sroseniemiec@gmail.com>
2017-05-09 12:19:26 -0400

    added events to calendar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,11 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    @events = Event.all
+  end
 
+  def show
+    @event = Event.find(params[:id])
   end
 
   def new
--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,9 @@ has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>"}, defa
 validates_presence_of :name, :description, :startdate, :enddate, :location, :cost
 validates :cost, numericality:{greater_than_or_equal_to: 0 }
  validates :location, inclusion: {in: ["Noho","Soho","Lower East Side", "Upper West  side", "Upper East Side", "West Village", "East Village", "Hell's Kitchen", "Financial District", "Wall St.", "Midtown", "Harlem", "Washington Heights", "Chelsea", "Tribeca", "Chinatown", "Murray Hill"]}
+
+ def start_time
+        self.startdate
+    end
+
 end
