9656c1735e37c322420adad132ebf81e8e21ba06
sarah niemiec <sroseniemiec@gmail.com>
2017-05-11 12:17:19 -0400

    map with all events

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -10,48 +10,43 @@ var map;
           center: {lat: 40.7549, lng: -73.9840},
           zoom: 12
         });
-        var marker = new google.maps.Marker({
-          position: {
-            lat: locationTest.lat,
-            lng: locationTest.lng
+        if (locationTest.constructor === Array) {
+          var infoWindow = new google.maps.InfoWindow();
 
-          },
-          map: map
-        });
+          var marker, i;
 
-        console.log(locationName);
-        var infoWindowOptions = {
+          for (i = 0; i < locationTest.length; i++){
+            marker = new google.maps.Marker({
+            position: {
+                lat: locationTest[i].lat,
+                lng: locationTest[i].lng
+              },
+            map: map
+           });
+           google.maps.event.addListener(marker,'click',(function(marker, i) {
+             return function() {
+               infowindow.setContent(locations[i][0]);
+               infoWindow.open(map, marker);
+             }
+           })(marker, i));
+
+          }
+
+        } else {
+          var marker = new google.maps.Marker({
+            position: {
+              lat: locationTest.lat,
+              lng: locationTest.lng
+            },
+            map: map
+          });
+
+          var infoWindowOptions = {
             content: locationName
           };
-        var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-            google.maps.event.addListener(marker,'click',function(e){
+          var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+          google.maps.event.addListener(marker,'click',function(e){
             infoWindow.open(map, marker);
           });
+        }
       }
-
-
-// all events map
-
-// var map;
-//       function initMap() {
-//         map = new google.maps.Map(document.getElementById('map'), {
-//           center: {lat: 40.7549, lng: -73.9840},
-//           zoom: 12
-//         });
-//         var marker = new google.maps.Marker({
-//           position: {
-//             lat: 40.7549,
-//             lng: -73.9840
-//
-//           },
-//           map: map
-//         });
-//
-//         var infoWindowOptionsFullMap = {
-//             content: "idk"
-//           };
-//         var infoWindowMap = new google.maps.InfoWindow(infoWindowOptions);
-//             google.maps.event.addListener(marker,'click',function(e){
-//             infoWindow.open(map, marker);
-//           });
-//       }
